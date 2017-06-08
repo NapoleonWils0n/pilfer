c3a2a2a1ebe9d332456c354a223927181f95a4a8
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-06-09 00:47:59 +0100

    regex

--- regex.py
+++ regex.py
@@ -11,9 +11,13 @@ def splitUrl(urldata):
     
     split url on slash or ampersand
     '''
+    localproxy = re.compile(r'^http://127.0.0.1[:0-9]?')
     amp = re.compile(r'(?=[&][a-zA-Z_]+=+[-a-zA-Z0-9.]?)')
     if '|' in urldata:
         print("match |")
+        return urldata
+    elif localproxy.search(urldata):
+        print("localproxy")
         return urldata
     elif amp.search(urldata):
         print("match &")
