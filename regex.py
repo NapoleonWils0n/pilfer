dc9648f2bcbab4ce65d86d453183933cde08febd
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-06-15 17:26:51 +0100

    dev

--- regex.py
+++ regex.py
@@ -19,11 +19,10 @@ def splitUrl(urldata):
         ud = urldata.split(r'|')
         a = ud[0] # url before |
         b = ud[1] # url after |
-        print(len(b))
         print(master(b))
 #        print(userAgent(b))
 #        print(cookie(b))
-        return (a, b)
+#        return (a, b)
     elif rtmp.match(urldata):
         print("rtmp")
         return urldata
@@ -34,30 +33,24 @@ def splitUrl(urldata):
         print("full url")
         return urldata
 
+# split string on = and store in dict
 def splitEquals(eq):
     return dict([v.split('=', 1) for v in eq if '=' in v])
-    
+
 urlparams = ''
 
 def match_func(pattern, urlparams):
-    def match_rule(urlparams):
-        return re.findall(pattern, urlparams)
-#        return splitEquals(re.findall(pattern, urlparams))
+    def match_rule(params):
+        return re.findall(pattern, params)
     return match_rule
     
-rules = []
-with open ('regex.txt', encoding='utf-8') as pattern_file:
-    for line in pattern_file:
-        pattern = line.split()
-        rules.append(match_func(pattern))
+patterns = \
+    (
+        ('u?User-a?Agent=[a-zA-Z0-9/.()\s,:;%+_-]+'),
+        ('[cC]ookie=[a-zA-Z0-9/&%_*~;=_\s]+')
+    )
 
-#patterns = \
-#    (
-#        ('u?User-a?Agent=[a-zA-Z0-9/.()\s,:;%+_-]+'),
-#        ('[cC]ookie=[a-zA-Z0-9/&%_*~;=_\s]+')
-#    )
-#
-#rules = [match_func(pattern, urlparams) for (pattern) in patterns]
+rules = [match_func(pattern, urlparams) for (pattern) in patterns]
 
 def master(urlparams):
     for match_func in rules:
