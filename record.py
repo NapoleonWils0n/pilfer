97e0e6491c3932df6b5518cd11b0d77c0f971e1d
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-08-13 17:26:13 +0100

    ffmpeg func

--- record.py
+++ record.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3 
 
 # record function
-def record(url, options=None, tflag="-t", duration=None):
+def ffmpeg(url, options=None, tflag=None, duration=None):
     recordingfile = 'recordingfile' 
     print("ffmpeg -hide_banner -stats -v panic", options, "-i", url, "-c:v copy -c:a copy", tflag, duration, recordingfile)
