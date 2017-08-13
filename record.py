9db9226ca0f872927709c8bedf1bf4ef95d3c900
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-08-13 14:15:10 +0100

    record

--- record.py
+++ record.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3 
 
 # record function
-def record(link, options, *args):
-            
-    print("ffmpeg -hide_banner -stats -v panic", options, "-i", link, "-c:v copy -c:a copy $tflag $duration $recordingfile")
+def record(url, options=None, tflag="-t", duration=None):
+    recordingfile = 'recordingfile' 
+    print("ffmpeg -hide_banner -stats -v panic", options, "-i", url, "-c:v copy -c:a copy", tflag, duration, recordingfile)
