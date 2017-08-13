baee55e7631381473b639542a9ca2347b9538b72
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-08-13 23:15:24 +0100

    func format

--- record.py
+++ record.py
@@ -3,5 +3,5 @@
 # record function
 def ffmpeg(url, options=None, tflag=None, duration=None):
     recordingfile = 'recordingfile' 
-    print("ffmpeg -hide_banner -stats -v panic", options, "-i", url, "-c:v copy -c:a copy", tflag, duration, recordingfile)
-    #print("ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options, tflag, duration, recordingfile))
+    #print("ffmpeg -hide_banner -stats -v panic", options, "-i", url, "-c:v copy -c:a copy", tflag, duration, recordingfile)
+    print("ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options, tflag, duration, recordingfile))
