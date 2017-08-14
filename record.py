bda02335ab4ac8dab8d29bb4b656c00a99689830
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-08-14 18:23:59 +0100

    ffmpeg subprocess

--- record.py
+++ record.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3 
 
+import shlex
+import pipes
+from subprocess import check_call
+
 # record function
 def ffmpeg(url, options=None, tflag=None, duration=None):
     recordingfile = 'recordingfile' 
     #print("ffmpeg -hide_banner -stats -v panic", options, "-i", url, "-c:v copy -c:a copy", tflag, duration, recordingfile)
-    print("ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options, tflag, duration, recordingfile))
+    #print("ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options, tflag, duration, recordingfile))
+
+    ffcmd = "ffmpeg -hide_banner -stats -v panic '{1}' -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options, tflag, duration, recordingfile)
+    print(shlex.split(ffcmd))
+
