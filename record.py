4c81e5edb801e9c153109b557605efbc9eca6b6e
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-08-16 18:55:05 +0100

    args

--- record.py
+++ record.py
@@ -7,10 +7,7 @@ from subprocess import check_call
 # record function
 def ffmpeg(url, options=None, tflag=None, duration=None):
     recordingfile = 'recordingfile' 
-    if options:
-        ffcmd = "ffmpeg -hide_banner -stats -v panic '{1}' -i {0} -c:v copy -c:a copy {2}".format(url, options, recordingfile)
-    else:
-        ffcmd = "ffmpeg -hide_banner -stats -v panic -i {0} -c:v copy -c:a copy {1}".format(url, recordingfile)
+    ffcmd = "ffmpeg -hide_banner -stats -v panic '{1}' -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options, tflag, duration, recordingfile)
 
     print(shlex.split(ffcmd))
 
