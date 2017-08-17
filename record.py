459a874c440cf46f4ab52f85710f3a597867971a
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-08-17 23:29:19 +0100

    removed quotes from var placeholder

--- record.py
+++ record.py
@@ -14,13 +14,14 @@ def ffmpeg(**kwargs):
     values = list(kwargs.values())
     # url from kwargs which is the dictionary passed to the function
     url = kwargs['url']
+
     ffcmd = "ffmpeg -hide_banner -stats -v panic -i {0} -c:v copy -c:a copy {1}".format(url, recordingfile)
 
     if 'user-agent' or 'referer' or 'cookie' or 'x-forward' in kwargs:
         # dict minus first time which is the url
         options = values[1:]
         options_join = ' '.join(options)
-        ffcmd = "ffmpeg -hide_banner -stats -v panic '{1}' -i {0} -c:v copy -c:a copy {2}".format(url, options_join, recordingfile)
+        ffcmd = "ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2}".format(url, options_join, recordingfile)
     
     if 'duration' in kwargs:
         tflag = kwargs['tflag']
@@ -28,7 +29,8 @@ def ffmpeg(**kwargs):
         # dict minus first time which is the url, and minus the last 2 items tflag and duration
         options = values[1:-2]
         options_join = ' '.join(options)
-        ffcmd = "ffmpeg -hide_banner -stats -v panic '{1}' -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options_join, tflag, duration, recordingfile)
+        ffcmd = "ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options_join, tflag, duration, recordingfile)
     
 
+    #print(repr(ffcmd))
     print(shlex.split(ffcmd))
