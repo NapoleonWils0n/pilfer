156b3912be8d49c927c24884ad5b1a4577adeecb
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-08-17 01:37:13 +0100

    referer

--- record.py
+++ record.py
@@ -4,12 +4,18 @@ import shlex
 import pipes
 from subprocess import check_call
 
-# record function
-def ffmpeg(url, options=None, tflag=None, duration=None):
+def ffmpeg(**kwargs):
     recordingfile = 'recordingfile' 
-    ffcmd = "ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options, tflag, duration, recordingfile)
     
+    print(kwargs)
+
+#    v = list(kwargs.values())
+#    urlslice = v[1:]
+#    options = ' '.join(urlslice)
+
+    #ffcmd = "ffmpeg -hide_banner -stats -v panic -i {url} -c:v copy -c:a copy".format(url)
     #ffcmd = "ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options, tflag, duration, recordingfile)
+    
+    #print(ffcmd)
 
     #print(shlex.split(ffcmd))
-
