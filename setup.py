28f05b338eb3dea57f572156bc6b1427740e45b7
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-09-03 20:46:11 +0100

    setup entry point

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ from distutils.core import setup
 setup(
     name = "segment",
     packages = ["segment"],
+    entry_points={
+          'console_scripts': [
+              'segment = segment.__main__:main'
+          ]
+    },
     version = "1.0",
     description = "tool to record videos from Kodi on the command line or from within Kodi",
     author = "NapoleonWils0n",
