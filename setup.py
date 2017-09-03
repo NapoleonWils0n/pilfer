22aaa8ac8883cba7a9839670245e346d2c22de5a
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-09-03 21:22:34 +0100

    setup

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@
 from distutils.core import setup
 
 setup(
-    name = "segment",
-    packages = ["segment"],
+    name='segment',
+    packages=['segment'],
+    version='0.0.1',
+    description='tool to record videos from Kodi on the command line or from within Kodi',
+    author='NapoleonWils0n',
+    url='https://github.com/NapoleonWils0n/segment',
+    keywords=['ffmpeg', 'rtmpdump', 'kodi'],
+    long_description='long description goes here',
+    license='GPL',
+    packages=find_packages(),
+    zip_safe=False,
     entry_points={
           'console_scripts': [
-              'segment = segment.__main__:main'
+              'segment=segment:main'
           ]
-    },
-    version = "1.0",
-    description = "tool to record videos from Kodi on the command line or from within Kodi",
-    author = "NapoleonWils0n",
-    url = "",
-    keywords = ["ffmpeg", "rtmpdump", "kodi"],
-    long_description = ""
+    }
+
 )
