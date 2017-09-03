4158b90a1d3af7905570def1f9ca33ad15793831
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-09-03 17:09:59 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name = "segment",
+    packages = ["segment"],
+    version = "1.0",
+    description = "tool to record videos from Kodi on the command line or from within Kodi",
+    author = "NapoleonWils0n",
+    url = "",
+    keywords = ["ffmpeg", "rtmpdump", "kodi"],
+    long_description = ""
+)
