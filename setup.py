85a65807e0de5bc80d84d9fd2c67b5d42d760dc8
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-09-03 22:03:51 +0100

    main

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     zip_safe=False,
     entry_points={
           'console_scripts': [
-              'segment=segment.__main__:main'
+              'segment=segment:main'
           ]
     }
 
