63f302a7a70dd82e8c5fe0f8507f52b6f119483b
t3rmin8tor <mark.barker2@sky.com>
2017-09-04 23:04:54 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,28 +1,13 @@
-import os
-import sys
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
 
+from setuptools import setup
 
-
-setup(
-    name='pilfer',
-    version='0.0.1',
-    description='tool to record videos from Kodi on the command line or from within Kodi',
-    author='NapoleonWils0n',
-    url='https://github.com/NapoleonWils0n/pilfer',
-    keywords=['ffmpeg', 'rtmpdump', 'kodi'],
-    long_description='long description goes here',
-    license='GPL',
-    packages=['pilfer'],
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'pilfer = pilfer:main'
-        ]
-    }
-)
+setup(name='pilfer',
+      version='0.0.1',
+      packages=['pilfer'],
+      entry_points={
+          'console_scripts': [
+              'pilfer = pilfer.__main__:main'
+          ]
+      },
+      )
