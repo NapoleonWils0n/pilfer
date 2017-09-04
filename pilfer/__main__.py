b5d3c9518c52cd171d77ca31ca60407e5cefb0fd
t3rmin8tor <mark.barker2@sky.com>
2017-09-04 23:03:52 +0100

    Add files via upload

---
+++ pilfer/__main__.py
@@ -0,0 +1,19 @@
+"""The main routine of my_project."""
+# http://go.chriswarrick.com/entry_points
+
+import sys
+
+
+def main(args=None):
+    """The main routine."""
+    if args is None:
+        args = sys.argv[1:]
+
+    print("This is the main routine.")
+    print("It should do something interesting.")
+
+    # Do argument parsing here (eg. with argparse) and anything else
+    # you want your project to do.
+
+if __name__ == "__main__":
+    main()
