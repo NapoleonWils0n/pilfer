031ce4eb70d484e88f319c3726018a2def7716ed
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-09-05 19:31:13 +0100

    main

---
+++ pilfer/__main__.py
@@ -0,0 +1,13 @@
+import sys
+def main(args=None):
+    """The main routine."""
+    if args is None:
+        args = sys.argv[1:]
+    import pilfer
+    
+
+    # Do argument parsing here (eg. with argparse) and anything else
+    # you want your project to do.
+
+if __name__ == "__main__":
+    pilfer.main()
