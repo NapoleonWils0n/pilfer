088f1630edacb18769d47e94de4182473a4d0e5e
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-09-06 00:43:31 +0100

    dev

--- pilfer/__main__.py
+++ pilfer/__main__.py
@@ -4,9 +4,9 @@ import sys
 
 def main(args=None):
     """The main routine."""
-#    if args is None:
-#        args = sys.argv[1:]
+    if args is None:
+        args = sys.argv[1:]
     import pilfer
 
 if __name__ == '__main__':
-    pilfer.main()
+    main()
