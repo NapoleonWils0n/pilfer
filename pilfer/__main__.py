166b0c8aca4a9da350f833b63023c0d6c8bf65de
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-09-06 00:16:09 +0100

    main stuff

--- pilfer/__main__.py
+++ pilfer/__main__.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
+import sys
 import pilfer
+
+def main(args=None):
+    """The main routine."""
+    if args is None:
+        args = sys.argv[1:]
+
+    print("This is the main routine.")
+    print("It should do something interesting.")
+
+    # Do argument parsing here (eg. with argparse) and anything else
+    # you want your project to do.
 
 if __name__ == '__main__':
     pilfer.main()
