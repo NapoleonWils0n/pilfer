c1c416b027593dacba53fec12790e729a8a44bab
Daniel J Wilcox <danieljwilcox@gmail.com>
2017-09-16 19:33:36 +0100

    mac fix

--- pilfer/record.py
+++ pilfer/record.py
@@ -15,6 +15,8 @@ def ffmpeg(**kwargs):
     time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     ext = 'mkv'
     recordingfile = os.path.join(home, desktop, 'video-{}.{}'.format(time, ext))
+    ffmpeg = 'ffmpeg'
+    rtmpdump = 'rtmpdump'
     
     if sys.platform.startswith('win32'):
         home = os.path.expanduser('~').replace('\\', '/')
@@ -22,21 +24,26 @@ def ffmpeg(**kwargs):
         time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
         ext = '.mkv'
         recordingfile = home + desktop + 'video-' + time + ext
-    
+    elif sys.platform.startswith('darwin'):
+        bin = 'bin'
+        ffmpeg = os.path.join(home, bin, 'ffmpeg')
+
     # get the values from the dictionary passed to the function
     values = list(kwargs.values())
     # url from kwargs which is the dictionary passed to the function
     url = kwargs['url']
 
-    ffcmd = "ffmpeg -hide_banner -stats -v panic -i {0} -c:v copy -c:a copy {1}".format(url, recordingfile)
+    ffcmd = "{0} -hide_banner -stats -v panic -i {1} -c:v copy -c:a copy {2}".format(ffmpeg, url, recordingfile)
+    #ffcmd = "ffmpeg -hide_banner -stats -v panic -i {0} -c:v copy -c:a copy {1}".format(url, recordingfile)
 
     if any(word in kwargs for word in ('user-agent', 'referer', 'cookie')):                 
         # dict minus first time which is the url
         options = values[1:]
         remove_bracket = str(options)[1:-1]
         options_join = ''.join(remove_bracket).replace('"', '')
-        ffcmd = "ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2}".format(url, options_join, recordingfile)
-    
+        #ffcmd = "ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2}".format(url, options_join, recordingfile)
+        ffcmd = "{0} -hide_banner -stats -v panic {2} -i {1} -c:v copy -c:a copy {3}".format(ffmpeg, url, options_join, recordingfile)
+
     if 'duration' in kwargs:
         tflag = kwargs['tflag']
         duration = kwargs['duration']
@@ -44,8 +51,9 @@ def ffmpeg(**kwargs):
         options = values[1:-2]
         remove_bracket = str(options)[1:-1]
         options_join = ''.join(remove_bracket).replace('"', '')
-        ffcmd = "ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options_join, tflag, duration, recordingfile)
-    
+        #ffcmd = "ffmpeg -hide_banner -stats -v panic {1} -i {0} -c:v copy -c:a copy {2} {3} {4}".format(url, options_join, tflag, duration, recordingfile)
+        ffcmd = "{0} -hide_banner -stats -v panic {2} -i {1} -c:v copy -c:a copy {3} {4} {5}".format(ffmpeg, url, options_join, tflag, duration, recordingfile)
+
 
     # split the ffmpeg command for subprocess
     ffsplit = shlex.split(ffcmd)
@@ -71,6 +79,8 @@ def rtmp(**kwargs):
     time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     ext = 'mkv'
     recordingfile = os.path.join(home, desktop, 'video-{}.{}'.format(time, ext))
+    ffmpeg = 'ffmpeg'
+    rtmpdump = 'rtmpdump'
     
     if sys.platform.startswith('win32'):
         home = os.path.expanduser('~').replace('\\', '/')
@@ -78,16 +88,21 @@ def rtmp(**kwargs):
         time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
         ext = '.mkv'
         recordingfile = home + desktop + 'video-' + time + ext
+    elif sys.platform.startswith('darwin'):
+        bin = 'bin'
+        ffmpeg = os.path.join(home, bin, 'ffmpeg')
+        rtmpdump = '/usr/local/bin/rtmpdump'
+
 
     # url from kwargs which is the dictionary passed to the function
     url = kwargs['url']
 
-    rtmpcmd = "rtmpdump -i '{0}' -o {1}".format(url, recordingfile)
+    rtmpcmd = "{0} -i '{1}' -o {2}".format(rtmpdump, url, recordingfile)
 
     if 'duration' in kwargs:
         tflag = kwargs['tflag']
         duration = kwargs['duration']
-        rtmpcmd = "rtmpdump -q -i '{0}' | ffmpeg -hide_banner -stats -v panic -i - -c:v copy -c:a copy {1} {2} {3}".format(url, tflag, duration, recordingfile)
+        rtmpcmd = "{0} -q -i '{1}' | {2} -hide_banner -stats -v panic -i - -c:v copy -c:a copy {3} {4} {5}".format(rtmpdump, ffmpeg, url, tflag, duration, recordingfile)
 
     print("running rtmp command:")
  
